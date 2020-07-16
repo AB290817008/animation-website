d7b39188dbee2de0a5c4aaad018ea3cd8595258f
AB290817008 <42501497+AB290817008@users.noreply.github.com>
2020-07-17 03:22:27 +0500

    ad

--- src/components/animation.js
+++ src/components/animation.js
@@ -2,13 +2,24 @@ import React from 'react'
 import './css/Animation.css';
 import sys from './../components/svg/syscoin-hero-v2.svg';
 import vidAnim from './../assets/videos-animations/syscoin-hero-animation-o.mp4'
+import {
+    Player,
+    ControlBar,
+    ReplayControl,
+    ForwardControl,
+    CurrentTimeDisplay,
+    TimeDivider,
+    PlaybackRateMenuButton,
+    VolumeMenuButton
+} from 'video-react';
 
-export default function Animation(){
 
-    
+export default function Animation() {
 
 
-    return(
+
+
+    return (
 
         <div className='animation'>
             <div className='anim-img'>
@@ -22,16 +33,16 @@ export default function Animation(){
                     </canvas>
                     <canvas width='1884' height='1600' className='seeThru-buffer' style={{display:'none'}}></canvas> */}
                     <Player ref={(player) => { this.player = player }}>
-  <source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" />
-</Player>
-                    
-                
-                
+                        <source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" />
+                    </Player>
+
+
+
                 </div>
 
             </div>
 
-                
+
         </div>
     )
 
